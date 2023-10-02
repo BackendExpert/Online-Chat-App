cfccbb3e35aa18aa276b96b81e7b279709eb4a43
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-02 20:22:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
 
                         <input type="submit" value="Login" class="btn btn-primary">
                     </form>
+                    <p>Don't have an Account ? <a href="">Create One</a></p>
                 </div>
             </div>
         </div>
