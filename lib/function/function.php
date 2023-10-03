0a02c452ab9d469a8b2b418dcc0940e870b3354f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-03 07:50:15 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -14,6 +14,20 @@
         }
         else{
             $select_user = "SELECT * FROM user_tbl WHERE username = '$username' || email = '$email";
+            $select_user_result = mysqli_query($con, $select_user);
+            $select_user_nor = mysqli_num_rows($select_user_result);
+
+            if($select_user_nor < 0){
+                return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                                <strong>SignUp Error!</strong> User Already Exists...!
+                                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                        </div>";
+            }
+            else{
+                $insert_user = "INSERT INTO user_tbl(username,email,pass,is_active,join_at)VALUES('$username','$email','$pass',1,NOW())";
+                $insert_user_result = mysqli_query($con, $insert_user);                
+            }
+            
         }
 
     }
