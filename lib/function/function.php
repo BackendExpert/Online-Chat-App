057b531ceb5ecf97f66323dd0137f79ac3b0d710
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-03 19:19:37 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -34,7 +34,7 @@
             else{
 
 
-                $insert_user = "INSERT INTO user_tbl(username,email,pass,user_type,is_active,join_at)VALUES('$username','$email',,'user',1,NOW())";
+                $insert_user = "INSERT INTO user_tbl(username,email,pass,user_type,is_active,join_at)VALUES('$username','$email','$pass','user',1,NOW())";
                 $insert_user_result = mysqli_query($con, $insert_user);
 
                 if($insert_user_result){
