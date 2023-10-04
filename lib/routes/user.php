2dc58d77b28862d8f9313601f644dba514bde2f5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-04 20:14:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/user.php
+++ lib/routes/user.php
@@ -2,3 +2,11 @@
 <?php include("../function/function.php"); ?>
 
 <link rel="stylesheet" href="../../css/style.css">
+
+<div class="container">
+    <div class="main-card">
+        <div class="header-card">
+            Name : JehanKnady
+        </div>
+    </div>
+</div>
