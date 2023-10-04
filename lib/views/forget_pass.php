015c4d20569787b3a4c9db6340fdd0191e876079
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-04 18:44:26 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/views/forget_pass.php
+++ lib/views/forget_pass.php
@@ -9,7 +9,7 @@
         <div class="col-lg-4">
             <div class="card login-card">
                 <div class="card-header">
-                    <h4><i class="fas fa-user-plus"></i>  Sign Up Here</h4>
+                    <h4><i class="fas fa-user-shield"></i>  Password Reset</h4>
                 </div>
                 <div class="card-body login-body">
                     <?php 
